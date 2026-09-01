aa8865230e766532f092af9d9e2c093b381a83b4
Lemon.Li <lemon.li@dida.com>
2026-09-01 14:43:41 +0800

    修复项目配置并新增哈希功能支持
    修复了 `Cike.Core.csproj` 中 `<Import>` 标签的格式问题。
    新增 `IHasher` 接口及其实现类 `Hasher`，提供字符串和对象数组的哈希功能。
    实现了对象序列化方法以支持复杂数据的哈希处理。

---
+++ src/Cike.Core/Hashers/Hasher.cs
@@ -0,0 +1,38 @@
+namespace Cike.Core.Hashers;
+
+/// <inheritdoc />
+public class Hasher : IHasher
+{
+    /// <inheritdoc />
+    public string Hash(string value)
+    {
+        var data = SHA256.HashData(Encoding.UTF8.GetBytes(value));
+        return Convert.ToHexString(data);
+    }
+
+    /// <inheritdoc />
+    public string Hash(object?[] values, JsonSerializerOptions? jsonSerializerOptions = null)
+    {
+        var strings = values.Select(e => Serialize(e, jsonSerializerOptions)).Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
+        var input = string.Join("|", strings);
+        return Hash(input);
+    }
+
+    public string Hash(params string?[] values)
+    {
+        var strings = values.Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
+        var input = string.Join("|", strings);
+        return Hash(input);
+    }
+
+    private string Serialize(object? payload, JsonSerializerOptions? jsonSerializerOptions = null)
+    {
+        if (payload == null)
+            return string.Empty;
+
+        if (payload is string s)
+            return s;
+
+        return JsonSerializer.Serialize(payload, payload.GetType(), jsonSerializerOptions);
+    }
+}
---
+++ src/Cike.Core/Hashers/IHasher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cike.Core.Hashers
+{
+    public interface IHasher
+    {
+        string Hash(string value);
+
+        public string Hash(object?[] values, JsonSerializerOptions? jsonSerializerOptions = null);
+
+        public string Hash(params string?[] values);
+    }
+}
